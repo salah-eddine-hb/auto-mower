7c10f54bf14641b3a1f6296fc5bc7254a995a4d9
salah <salahelf@gmail.com>
2018-04-27 21:34:00 +0100

    Add comment for the validator service

--- src/app/validator.service.ts
+++ src/app/validator.service.ts
@@ -5,10 +5,14 @@ export class ValidatorService {
 
   constructor() { }
 
-  //check empty file, file with errors, ...
+  /**
+  * Check the input file for validity
+  * @param data string 
+  * @returns erros array (empty if there is no error)
+  */
   public validate(content: string): string[]{
     const lines = content.toString().split('\n');
-    let errors: string[] =  [];
+    let errors: string[] = [];
     let index = 1;
     for (let line of lines) {
       if (index == 1) {
@@ -28,18 +32,36 @@ export class ValidatorService {
     return errors;
   }
 
+  /**
+  * Check the upper right corner, the corner
+  * should respect the regex /^[0-9]{2}$/
+  * @param data string
+  * @returns test result
+  */
   private checkUpperRightCorner(data: string): boolean{
     let regex = /^[0-9]{2}$/g
     let result = regex.test(data);
     return result;
   }
 
+  /**
+  * Check starting position and orientation 
+  * they should respect /^[0-9]{2}[NEWS]{1}$/
+  * @param data string
+  * @returns test result
+  */
   private checkStartingPositionAndOrientation(data: string): boolean{
     let regex = /^[0-9]{2}[NEWS]{1}$/g
     let result = regex.test(data);
     return result;
   }
 
+  /**
+  * Check Instructions
+  * it should respect /^[ADG]+$/
+  * @param data string
+  * @returns test result
+  */
   private checkInstructions(data: string): boolean{
     let regex = /^[ADG]+$/g
     let result = regex.test(data);
