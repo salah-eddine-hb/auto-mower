7bd78801f0f9f3e23d2d507f4a4e0b44a3aad2ff
salah <salahelf@gmail.com>
2018-05-03 16:42:33 +0100

    Make the openFile fucntion return a promise to facilate the test

--- src/app/file-uploader/file-uploader.component.ts
+++ src/app/file-uploader/file-uploader.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ValidatorService } from '../validator.service';
 import { ExecutorService } from '../executor.service';
 import { Mower } from '../Mower';
+import { resolve } from 'path';
+import { reject } from 'q';
 
 @Component({
   selector: 'app-file-uploader',
@@ -18,15 +20,29 @@ export class FileUploaderComponent implements OnInit {
   ngOnInit() {
   }
 
-  openFile(files: FileList) {
+  openFile(file: File) {
+    return new Promise((resolve, reject) => {
     let reader = new FileReader();
-
-    reader.onload = () => {
-        const data = reader.result;
-        this.errors = this.validatorService.validate(data);
-        if(this.errors.length == 0) 
-          this.mowers = this.executorService.loadMowers(data);
+    if(file){
+      this.errors = undefined;
+      this.mowers = undefined;
+      let onload = reader.onload = () => {
+          const data = reader.result;
+          this.errors = this.validatorService.validate(data);
+          if(this.errors.length == 0) 
+            this.mowers = this.executorService.loadMowers(data);
+          resolve(this.errors);
+      }
+      reader.readAsText(file);
+    }else{
+      this.errors = ['Plz upload one valid file'];
+      resolve(this.errors);
     }
-    reader.readAsText(files[0]);
-    };
+  });
   }
+
+  get Errors(){
+    return this.errors;
+  }
+
+}
