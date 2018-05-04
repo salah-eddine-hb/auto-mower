a08deab18627b194a9da8155785e7585d87dfdbd
salah <salahelf@gmail.com>
2018-05-04 13:26:44 +0100

    Add errors description for error enum

--- src/app/error.enum.ts
+++ src/app/error.enum.ts
@@ -1,2 +1,6 @@
 export enum Error {
+    ERROR_FILE='Plz upload a file',
+    ERROR_CORNER='Error parsing the corner value',
+    ERROR_POSITION_ORIENTATION='Error parsing the position/oriantation value',
+    ERROR_INSTRUCTION='Error parsing instruction value'
 }
